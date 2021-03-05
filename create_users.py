7b64bdec85e136e47dd711a99de12a1af75d68ed
Stephen Ward <s.ward@auckland.ac.nz>
2021-03-05 19:09:53 +1300

    Added some parsing of results for user creation

--- create_users.py
+++ create_users.py
@@ -32,13 +32,54 @@ def add_users_to_groups_even_new(group_name, file_name=None, user_type=2, debug=
     add_to_group_results = zoom_groups.add_users_to_group_from_email_list_file(group_name, file_name)
     change_type_results = zoom_groups.change_user_type_for_group(group_name, user_type)
     if debug:
+        already_exist = []
+        created = []
+        added = []
         if even_new:
             # noinspection PyUnboundLocalVariable
             for create_result in create_results:
-                print(create_result.content)
+                result_content = json.loads(create_result.content)
+                if 'email' in result_content:
+                    created.append(result_content['email'])
+                elif 'message' in result_content:
+                    already_exist.append(result_content['message'].replace("User already in the account: ", ""))
         for add_to_group_result in add_to_group_results:
-            print(add_to_group_result.content)
-        print(change_type_results)
+            result_content = json.loads(add_to_group_result.content)
+            if 'ids' in result_content:
+                if result_content['ids'] != "":
+                    added.append(result_content['ids'])
+        created_count = len(created)
+        exist_count = len(already_exist)
+        added_count = len(added)
 
+        output = ""
+        if already_exist:
+            if len(already_exist) > 1:
+                output += f"The following {exist_count} users already exist:\n"
+            else:
+                output += "The following user already exists:\n"
+            for entry in already_exist:
+                output += f"{entry}\n"
+            output += "\n"
+        if created:
+            if len(created) > 1:
+                output += f"The following {created_count} users have been invited:\n"
+            else:
+                output += "The following user has been invited:\n"
+            for entry in created:
+                output += f"{entry}\n"
+            output += "\n"
+        if added:
+            if len(added) > 1:
+                output += f"The following {added_count} user ids have been added to the group:\n"
+            else:
+                output += "The following user id has been added to the group:\n"
+            for entry in added:
+                output += f"{entry}\n"
+            output += "\n"
+        if change_type_results:
+            output += "User type(s) were successfully updated\n"
+
+        print(output)
 
 # add_users_to_groups_even_new('Group Name', debug=True)
